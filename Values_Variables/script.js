dcc9f71fa681768ee204e1bdd343086aabf271d4
sbrown7861 <skylerbrown@Skylers-MacBook-Pro.local>
2014-06-10 22:28:14 -0700

    Values and variables commit
    code for values, variables, boolean, and arrays

---
+++ Values_Variables/script.js
@@ -0,0 +1,17 @@
+var yearBorn = 1992;
+
+console.log(yearBorn);
+alert(yearBorn);
+
+var age = 22;
+console.log(age);
+
+var date; //declaration
+date = 10; //definition
+
+var isStudent = true; //Boolean Variable
+
+var firstName = "Skyler";
+
+var bandNames = ["August Burns Red", "Guns n Roses", "Black Sabbath" ];
+console.log(bandNames);
