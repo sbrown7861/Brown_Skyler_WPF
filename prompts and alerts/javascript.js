f7d43417ed7d79fd9f59ed99c3bb3790cd9b0871
sbrown7861 <skylerbrown@Skylers-MacBook-Pro.local>
2014-06-11 19:42:27 -0700

    Using prompts and alerts
    prompts and alerts with labels.

---
+++ prompts and alerts/javascript.js
@@ -0,0 +1,11 @@
+var userInput = prompt("enter your year of birth");
+console.log(userInput);
+
+var width = prompt("width of a triangle");
+var height = prompt("please enter height of triangle");
+var area = width * height;
+console.log(area);
+
+//show prompt information to user using alert.
+
+alert('area of your triangle is' + area);
