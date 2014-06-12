ea4457a94c8378976eaf2c4444828e86e859b2e2
sbrown7861 <skylerbrown@Skylers-MacBook-Pro.local>
2014-06-11 19:13:13 -0700

    Expression, casts, order of operations
    expressions, casts, order of operations, and use of string arrays

---
+++ Expressions/javascript.js
@@ -0,0 +1,38 @@
+var a = 2;
+console.log(a);
+a = a+3;
+console.log(a);
+
+var yearBorn = 1992;
+var age = 2014 - yearBorn;
+console.log(age);
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+var average = (quiz1 + quiz2 + quiz3 +quiz4)/4;
+console.log(average);
+
+var length = 7;
+var width = 10;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
+
+var orangeBins = [234, 567, 883];
+var total = orangeBins[0]+ orangeBins[1] + orangeBins[2];
+console.log(total);
+
+var firstName = 'Skyler';
+var lastName = 'Brown';
+var fullName = firstName +' '+ lastName;
+console.log(fullName);
+
+var stringVar = '6';
+var result = 7 + Number(stringVar);
+console.log(result);
+
+var d = 3;
+d = d + 4;
+console.log (d);
