fb24e289ebfb16383051144af5d7ed593fa5fd8e
sbrown7861 <skylerbrown@Skylers-MacBook-Pro.local>
2014-06-17 18:48:28 -0700

    Screencast of conditionals
    Reference this for lay out of conditionals. or conditional is used for
    either part of conditions being true and is represented by ||

---
+++ conditionals_practice_screencast/javascript.js
@@ -0,0 +1,33 @@
+var oldEnough = true;
+
+if(oldEnough)
+    {console.log("You can ride the coaster");
+
+}
+
+var kidHeight = 48;
+
+if(kidHeight > 48)
+    {console.log("you can ride the coaster");
+
+    }
+
+else{
+        console.log("Sorry kid, you got some growing to do!");
+    }
+
+var buget= 300;
+var smartPhonePrice= 200;
+var paycheck= 300;
+
+if(smartPhonePrice < buget && paycheck >= 300){ console.log("We can buy the phone");
+
+}else{ console.log("No phone for you!");
+
+}
+
+var age = 10;
+var book;
+
+book=(age < 10) ? "Green Eggs and Ham" : "The Time Machine";
+console.log (book);
